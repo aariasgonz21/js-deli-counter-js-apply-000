9c3f02be50d20eeb801dc705e15db366ffcb82e9
Ana Arias-Gonzalez <ana.ariasgonz@gmail.com>
2018-10-20 22:51:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,21 +27,3 @@ function currentLine(katzDeliLine){
     return `The line is currently:${list}`;
   }
 }
-
-/*
- Try #1: 
-  else{
-    for(var i = 0; i < katzDeliLine.length; i++){
-      return `The line is currently ${i + 1}. ${katzDeliLine[i]}`;
-    }
-    ***Only returned one name not the name in a row***
-    
-    Try #2:
-    else{
-    for(var i = 0; i < katzDeliLine.length; i++){
-      list += `${i + 1}. ${katzDeliLine[i]}, `
-    }
-    return `The line is currently: ${list}`
-  }
-  ***Returned list but added ',' to the last name
-*/
