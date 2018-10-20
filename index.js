5723a73a2e690d75300472f9ddeec945991646ac
Ana Arias-Gonzalez <ana.ariasgonz@gmail.com>
2018-10-20 22:32:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ function currentLine(katzDeliLine){
   if(katzDeliLine.length === 0){
     return "The line is currently empty."
   }
+  
   else{
     for(var i = 0; i < katzDeliLine.length; i++){
       punctuation += (i == katzDeliLine.length -1)? '' : ','
