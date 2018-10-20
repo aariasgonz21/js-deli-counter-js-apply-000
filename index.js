203d53a1a79dce31c4bf8e6c9affb657a056d39f
Ana Arias-Gonzalez <ana.ariasgonz@gmail.com>
2018-10-20 22:01:16 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,35 @@
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+}
+
+function nowServing(katzDeliLine){
+  if(katzDeliLine.length === 0){
+    return "There is nobody waiting to be served!";
+  }
+  else{
+    return `Currently serving ${katzDeliLine.shift()}.`;
+  }
+}
+
+function currentLine(katzDeliLine){
+  var list = ``;
+  if(katzDeliLine.length === 0){
+    return "The line is currently empty."
+  }
+  else{
+    for(var i = 0; i < katzDeliLine.length; i++){
+      list += `${i + 1}. ${katzDeliLine[i]}, `
+    }
+    return `The line is currently: ${list}`
+  }
+}
+
+/*
+ Try #1: 
+  else{
+    for(var i = 0; i < katzDeliLine.length; i++){
+      return `The line is currently ${i + 1}. ${katzDeliLine[i]}`;
+    }
+    ***Only returned one name not the name in a row***
+*/
