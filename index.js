336bae855a62c87c0a0d386f7dc9106f120dd755
Ana Arias-Gonzalez <ana.ariasgonz@gmail.com>
2018-10-20 22:20:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,13 +13,16 @@ function nowServing(katzDeliLine){
 }
 
 function currentLine(katzDeliLine){
+  let message = ``;
+  
   if(katzDeliLine.length === 0){
     return "The line is currently empty."
   }
   else{
-    let message = ``;
     for(var i = 0; i < katzDeliLine.length; i++){
-      
+      if(katzDeliLine[i] === katzDeliLine.length - 1){
+        
+      }
     }
   }
 }
